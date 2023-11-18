4612fe654bebdf74a9f7e08402c42b9b80484b77
Balázs Albert <albertbalzs@gmail.com>
2023-11-18 19:34:54 +0100

    section.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Section from './components/section'
+
+const content = {
+  title: 'Movie application',
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Movie application</h1>
+      <Section title={content.title}>
+        <div>Content</div>
+      </Section>
     </main>
   )
 }
