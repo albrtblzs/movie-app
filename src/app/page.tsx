6c345de66219064a1dc40fcabee38c333e62e1d9
Balázs Albert <albertbalzs@gmail.com>
2023-11-18 19:08:09 +0100

    globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
-  const defaultClassName = 'text-red-500'
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Movie Application
+      <h1>Movie application</h1>
     </main>
   )
 }
