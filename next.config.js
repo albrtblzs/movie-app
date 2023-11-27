1dbc40764b1c34426ccf3dd3bb20c897ef5352d6
Balázs Albert <albertbalzs@gmail.com>
2023-11-27 17:13:32 +0100

    disable caching on vercel

--- next.config.js
+++ next.config.js
@@ -3,21 +3,6 @@ const nextConfig = {
   images: {
     domains: ['image.tmdb.org']
   },
-  async headers() {
-    const headers = [
-      {
-        headers: [
-          {
-            key: "Cache-Control",
-            value:
-              "s-maxage=120, stale-while-revalidate",
-          },
-        ],
-        source: "/:path*",
-      },
-    ];
-    return headers;
-  },
 }
 
 module.exports = nextConfig
